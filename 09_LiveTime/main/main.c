dcc4d2d6d3e403bd3777ca2b05576f3c50924caf
Damian Podraza <damian.podraza@gmail.com>
2025-11-29 20:36:16 +0100

    add 09

---
+++ 09_LiveTime/main/main.c
@@ -0,0 +1,24 @@
+/*
+Encoder bez przerwań z kolejką pomiędzy taskami.
+*/
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "esp_timer.h"
+#include "esp_log.h"
+
+void app_main(void)
+{
+    while (1)
+    {
+        uint64_t uptime_us = esp_timer_get_time();
+        uint64_t uptime_ms = uptime_us / 1000;
+        uint64_t tick =  xTaskGetTickCount();
+        printf("time stamp -> %llu[ms] %llu[us] %llu[tick]\n", uptime_ms, uptime_us, tick);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+}
