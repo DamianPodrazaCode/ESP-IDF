6d76023f557215e6bfea70e7b6e24479c2e121a8
Damian Podraza <damian.podraza@gmail.com>
2025-12-03 20:09:28 +0100

    Update main.c

--- 14_TaskNotifications/main/main.c
+++ 14_TaskNotifications/main/main.c
@@ -7,30 +7,19 @@ Powiadomienia Zadań pozwalają na komunikację bezpośrednią - Zadanie A -> (s
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-// Uchwyt do zadania odbiorcy (musimy wiedzieć, kogo "szturchnąć")
-static TaskHandle_t s_receiver_handle = NULL;
+static TaskHandle_t s_receiver_handle = NULL; // Uchwyt do zadania odbiorcy
 
-// --- ZADANIE 1: NADAWCA (Sensor) ---
 void task_sender(void *pvParam)
 {
-    while(1) {
+    while (1)
+    {
         // 1. Symulujemy odczyt z czujnika (losowa liczba 20-30)
         uint32_t temperature = 20 + (rand() % 10);
-        
         printf("[Sender] Wysyłam temperaturę: %lu\n", temperature);
 
-        // 2. WYSYŁAMY POWIADOMIENIE
-        // xTaskNotify(
-        //    uchwyt_adresata, 
-        //    wartość, 
-        //    akcja
-        // )
-        
-        // eSetValueWithOverwrite: Nadpisz starą wartość w "kieszeni" nową.
-        // Działa jak jednoelementowa kolejka, która zawsze przyjmuje najnowsze dane.
-        xTaskNotify(s_receiver_handle, temperature, eSetValueWithOverwrite);
+        // xTaskNotify(uchwyt_adresata, wartość, akcja);
+        xTaskNotify(s_receiver_handle, temperature, eSetValueWithOverwrite); // eSetValueWithOverwrite: Nadpisz starą wartość, nową.
 
-        // Symulacja pracy co 2 sekundy
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
@@ -42,7 +31,8 @@ void task_receiver(void *pvParam)
 
     printf("[Receiver] Czekam na dane...\n");
 
-    while(1) {
+    while (1)
+    {
         // 3. ODBIERAMY POWIADOMIENIE
         // xTaskNotifyWait(
         //    bity_do_wyczyszczenia_na_wejsciu,
@@ -50,16 +40,18 @@ void task_receiver(void *pvParam)
         //    wskaźnik_na_zmienną_do_zapisu,
         //    czas_oczekiwania
         // )
-        
+
         // 0x00, 0xFFFFFFFF -> Nie czyść nic przed sprawdzeniem, wyczyść wszystko po odczycie.
         // To klasyczna konfiguracja do odczytu wartości liczbowej.
         BaseType_t result = xTaskNotifyWait(0x00, 0xFFFFFFFF, &received_value, portMAX_DELAY);
 
-        if (result == pdTRUE) {
+        if (result == pdTRUE)
+        {
             printf("[Receiver] Odebrano! Temperatura: %lu 'C\n", received_value);
-            
+
             // Reakcja na wysoką temperaturę
-            if (received_value > 28) {
+            if (received_value > 28)
+            {
                 printf("[Receiver] ALARM! Za gorąco!\n");
             }
         }
