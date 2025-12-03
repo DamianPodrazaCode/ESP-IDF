b9c4e29f2bad78269cad012bdd93420cefd7421f
Damian Podraza <damian.podraza@gmail.com>
2025-12-03 20:30:09 +0100

    Update main.c

--- 01_GPIO_LED/main/main.c
+++ 01_GPIO_LED/main/main.c
@@ -9,31 +9,28 @@
 
 void app_main(void)
 {
-    ESP_LOGI("111","222");  // Macro to output logs at ESP_LOG_INFO level.
-    printf("Start app_main !!!!\n"); // Standardowe wyjście uart0, czyli to którym programujemy
+    // ESP_LOGI("111","222");  // makro do wysyłania infa do konsoli na zielono
+    printf("Start app_main !!!!\n"); // wyjscie std, w tym przypadku konsola na biało
 
-    // prosty sposób
-    // gpio_reset_pin(BLINK_GPIO);                       
+    // GPIO prosty sposób
+    // gpio_reset_pin(BLINK_GPIO);
     // gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
-    // pełny sposób
-	gpio_config_t io_conf;
-	io_conf.pin_bit_mask = (1ULL << BLINK_GPIO); 
-	io_conf.mode = GPIO_MODE_OUTPUT;              
-	io_conf.pull_up_en = GPIO_PULLUP_DISABLE;    
-	io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-	io_conf.intr_type = GPIO_INTR_DISABLE;       
-	gpio_config(&io_conf);     
-    
-    // siła wysterowania, prędkość zbocza
+    // GPIO pełny sposób
+    gpio_config_t io_conf;
+    io_conf.pin_bit_mask = (1ULL << BLINK_GPIO);
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    gpio_config(&io_conf);
+
+    // GPIO siła wysterowania, prędkość zbocza
     // GPIO_DRIVE_CAP_0  //ok. 5 mA - narastanie 100ns
-	// GPIO_DRIVE_CAP_1  //ok. 10 mA (często domyślny)
-	// GPIO_DRIVE_CAP_2  //(lub GPIO_DRIVE_CAP_DEFAULT): ok. 20 mA
-	// GPIO_DRIVE_CAP_3  //ok. 40 mA - 40ns    
-    esp_err_t err = gpio_set_drive_capability(BLINK_GPIO, GPIO_DRIVE_CAP_3);
-    if (err == ESP_OK)
-    {
-    }
+    // GPIO_DRIVE_CAP_1  //ok. 10 mA (często domyślny)
+    // GPIO_DRIVE_CAP_2  //(lub GPIO_DRIVE_CAP_DEFAULT): ok. 20 mA
+    // GPIO_DRIVE_CAP_3  //ok. 40 mA - 40ns
+    gpio_set_drive_capability(BLINK_GPIO, GPIO_DRIVE_CAP_3);
 
     while (1)
     {
@@ -45,12 +42,11 @@ void app_main(void)
         // gpio_set_level(BLINK_GPIO, 0);
         // gpio_set_level(BLINK_GPIO, 1);
         // gpio_set_level(BLINK_GPIO, 0);
-        // vTaskDelay(10 / portTICK_PERIOD_MS); // Czekaj żeby nie zawiesić rtos
+        // vTaskDelay(10 / portTICK_PERIOD_MS);
 
         gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(100 / portTICK_PERIOD_MS); // Czekaj żeby nie zawiesić rtos
+        vTaskDelay(100 / portTICK_PERIOD_MS);
         gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(100 / portTICK_PERIOD_MS); // Czekaj żeby nie zawiesić rtos
-
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
